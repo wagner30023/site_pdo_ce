415ec5ba3943ff37dcba853ff3daac216871478f
wagner30023 <wagner30023@gmail.com>
2015-01-22 16:10:17 -0200

    Update Contato.DAO.php

--- site_pdo_ce/lib/Contato.DAO.php
+++ site_pdo_ce/lib/Contato.DAO.php
@@ -5,10 +5,11 @@
  *
  * @author carlos
  */
+ 
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+ 
 require_once ('PDOConnectionFactory.php');
-
-
-
 
 class ContatoDAO extends PDOConnectionFactory {
 
@@ -18,11 +19,17 @@ class ContatoDAO extends PDOConnectionFactory {
             $sql = "INSERT INTO contato(id_contato,nome,email,assunto,mensagem) VALUES(?,?,?,?,?)";
             $stmt = $pdo->prepare($sql);
             
-            $stmt->bindParam(1, $contato->getId_contato(),  PDO::PARAM_INT);
-            $stmt->bindParam(2, $contato->getNome()  , PDO::PARAM_STR);
-            $stmt->bindParam(3, $contato->getEmail(),PDO::PARAM_STR);
-            $stmt->bindParam(4, $contato->getAssunto(),PDO::PARAM_STR);
-            $stmt->bindParam(5, $contato->getMensagem(),PDO::PARAM_STR);
+            $id_contato = $contato->getId_contato();
+            $nome = $contato->getNome();
+            $email = $contato->getEmail();
+            $assunto = $contato->getAssunto();
+            $mensagem = $contato->getMensagem();
+
+            $stmt->bindParam(1, $id_contato, PDO::PARAM_INT);
+            $stmt->bindParam(2, $nome, PDO::PARAM_STR);
+            $stmt->bindParam(3, $email, PDO::PARAM_STR);
+            $stmt->bindParam(4, $assunto, PDO::PARAM_STR);
+            $stmt->bindParam(5, $mensagem, PDO::PARAM_STR);
 
               $stmt->execute();
         } catch (Exception $exc) {
